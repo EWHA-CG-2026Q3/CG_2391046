dc13519e1f6a745b21eb1e06b43befe7c4a841b3
김유림 <2391046@ewha.ac.kr>
2026-09-13 01:03:09 +0900

    feat: S02 폴더 정리 및 좌표계 스크립트 적용

---
+++ Assets/S03/S03_CustomPolygonMesh_Square.cs
@@ -0,0 +1,21 @@
+﻿// 1차 커밋용 코드 상태
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class S03_CustomPolygonMesh_Square : MonoBehaviour
+{
+    void Start()
+    {
+        // 정점 5개 정의
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3(0f, 1.5f, 0f),
+            new Vector3(-1f, 0.5f, 0f),
+            new Vector3(1f, 0.5f, 0f),
+            new Vector3(-0.7f, -1f, 0f),
+            new Vector3(0.7f, -1f, 0f)
+        };
+
+        int[] triangles = new int[] { };
+    }
+}
---
+++ Assets/S03/S03_SingleTriangleMesh_Finish.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class S03_SingleTriangleMesh_Finish : MonoBehaviour
+{
+    void Start()
+    {
+        // 1. 삼각형을 구성하는 정점(Vertex) 3개
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3(0f, 0f, 0f),
+            new Vector3(0f, 1f, 0f),
+            new Vector3(1f, 0f, 0f)
+        };
+
+        // 2. 삼각형을 이루는 정점 인덱스 (시계 방향)
+        int[] triangles = new int[]
+        {
+            0, 1, 2
+        };
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+
+        GetComponent<MeshFilter>().mesh = mesh;
+        GetComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Universal Render Pipeline/Lit"));
+    }
+}
