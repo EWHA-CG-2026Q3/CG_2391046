9af5da3950d0d1acd6b652e604e8f3426d27dc7a
김유림 <2391046@ewha.ac.kr>
2026-09-19 17:48:40 +0900

    chore: S03 폴더 생성 및 기본 에셋 추가

--- Assets/S03/S03_SingleTriangleMesh_Finish.cs
+++ Assets/S03/S03_SingleTriangleMesh_Finish.cs
@@ -5,19 +5,19 @@ public class S03_SingleTriangleMesh_Finish : MonoBehaviour
 {
     void Start()
     {
-        // 1. 삼각형을 구성하는 정점(Vertex) 3개
         Vector3[] vertices = new Vector3[]
         {
-            new Vector3(0f, 0f, 0f),
-            new Vector3(0f, 1f, 0f),
-            new Vector3(1f, 0f, 0f)
+            new Vector3(0f, 0f, 0f), // 0
+            new Vector3(1f, 0f, 0f), // 1
+            new Vector3(1f, 1f, 0f), // 2
+            new Vector3(0f, 1f, 0f), // 3
+            new Vector3(0f, 0f, 1f), // 4
+            new Vector3(1f, 0f, 1f), // 5
+            new Vector3(1f, 1f, 1f), // 6
+            new Vector3(0f, 1f, 1f), // 7
         };
 
-        // 2. 삼각형을 이루는 정점 인덱스 (시계 방향)
-        int[] triangles = new int[]
-        {
-            0, 1, 2
-        };
+        int[] triangles = new int[3] { 4, 5, 6 };
 
         Mesh mesh = new Mesh();
         mesh.vertices = vertices;
