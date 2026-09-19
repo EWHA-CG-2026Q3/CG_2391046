105fc0eebf5a1f8578f8027c8c988355e6bb63f6
김유림 <2391046@ewha.ac.kr>
2026-09-19 19:13:10 +0900

    refactor: 정점 6개 육각형 메쉬 구조로 확장 및 구현

--- Assets/S03/S03_CustomPolygonMesh_Square.cs
+++ Assets/S03/S03_CustomPolygonMesh_Square.cs
@@ -5,22 +5,24 @@ public class S03_CustomPolygonMesh_Square : MonoBehaviour
 {
     void Start()
     {
-        // TODO 1: 정점 좌표 5개 정의 (오각형)
+        // 정점 좌표 6개 정의 (정육각형)
         Vector3[] vertices = new Vector3[]
         {
-            new Vector3(0f, 1f, 0f),       // 0: 상단
-            new Vector3(0.95f, 0.31f, 0f),  // 1: 우상단
-            new Vector3(0.59f, -0.81f, 0f), // 2: 우하단
-            new Vector3(-0.59f, -0.81f, 0f),// 3: 좌하단
-            new Vector3(-0.95f, 0.31f, 0f)  // 4: 좌상단
+            new Vector3(0f, 1f, 0f),         // 0: 상단
+            new Vector3(0.87f, 0.5f, 0f),    // 1: 우상단
+            new Vector3(0.87f, -0.5f, 0f),   // 2: 우하단
+            new Vector3(0f, -1f, 0f),        // 3: 하단
+            new Vector3(-0.87f, -0.5f, 0f),  // 4: 좌하단
+            new Vector3(-0.87f, 0.5f, 0f)    // 5: 좌상단
         };
 
-        // TODO 2: 정점 3개씩 묶어 삼각형 3개 구성
+        // 정점 3개씩 묶어 삼각형 4개 구성
         int[] triangles = new int[]
         {
             0, 1, 2, // 첫 번째 삼각형
             0, 2, 3, // 두 번째 삼각형
-            0, 3, 4  // 세 번째 삼각형
+            0, 3, 4, // 세 번째 삼각형
+            0, 4, 5  // 네 번째 삼각형
         };
 
         Mesh mesh = new Mesh();
